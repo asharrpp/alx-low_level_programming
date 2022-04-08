bd7115523ffa766136ac49189f07bd1c6ffef721
asharrpp <akinpelu212@gmail.com>
2022-04-08 12:07:14 -0700

    Task 3 corrected

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,20 @@
  */
 int main(void)
 {
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-putchar(c);
-}
-for (c = 'A'; c <= 'Z'; c++)
-{
-putchar(c);
-}
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 
-putchar('\n');
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
 
-return (0);
+	putchar('\n');
+
+	return (0);
+
 }
